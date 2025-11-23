8a49220f2c3501f95647703ae542755b344a6b44
Zenithar <borns1256@gmail.com>
2025-11-23 08:57:25 -0500

    funcion provisional:' ver tarea' implementada

--- Lista_de_Tareas/core.py
+++ Lista_de_Tareas/core.py
@@ -5,9 +5,15 @@ class Gestor_De_Tareas:
         
     def agregarTarea(self,Tarea):
         self.listaT.append(Tarea)
-    
-    
         
+    def verTareas(self):
+        posicion = 1
+        print("\nLas tareas son: ")
+        for T in self.listaT:
+            print(f"{posicion}-{T}")
+            posicion += 1
+            
+    
 class Tarea:
     def __init__(self, descripcion):
         self.descripcion = descripcion
