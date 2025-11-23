20e2abcb609d67680fa5b29bcc0352e3c972f7f4
Zenithar <borns1256@gmail.com>
2025-11-23 10:46:26 -0500

    actualizacion grande: diccionario para elegir implementado, ajustes en el sistema de impresion en consola, creado bucle de eleccion, aplicada verificacion de errores basica, funcion agregar tarea testeada: OK

--- Lista_de_Tareas/main.py
+++ Lista_de_Tareas/main.py
@@ -1,16 +1,40 @@
-from Lista_de_Tareas.core import * 
+from core import * 
+
+flag = True
+while(flag):
+    print("--------------Gestor de tareas--------------")
+    print("Que desa hacer?")
+    print("1-Agregar Tarea")
+    print("2-Ver Tareas")
+    print("3-Marcar Tarea como completada")
+    print("4-Eliminar Tarea")
+    print("5-Guardar datos")
+    print("6-Cargar datos")
+    print("7-Salir")
+    eleccion = input("Elija una opcion: ")
+    
+    if int(eleccion) < 1 or int(eleccion) > 7:
+        print("\n!!!!!!!FUERA DE RANGO!!!!!!")
+    elif eleccion == "7":
+        flag = False
+        
+    gestor = Gestor_De_Tareas()
+    Opciones = {
+    "1": lambda: gestor.agregarTarea(),
+    "2": lambda: gestor.verTareas(),
+    "3": lambda: gestor.marcarCompletada(),
+    "4": lambda: gestor.eliminaTarea()
+    }
+
+    action = Opciones.get(eleccion,lambda: print(" "))
+    action()
+    
+    
 
 
-print("--------------Gestor de tareas--------------")
-print("\nQue desa hacer?")
-print("1-Agregar Tarea")
-print("2-Ver Tareas")
-print("3-Marcar Tarea como completada")
-print("4-Eliminar Tarea")
-print("5-Guardar datos")
-print("6-Cargar datos")
+        
+        
 
-eleccion = int(input("Elija una opcion"))
 
 
 
