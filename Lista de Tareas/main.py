6b1eeeaf99fa23deb66894197978051aa7dd8280
Zenithar <borns1256@gmail.com>
2025-11-23 00:04:02 -0500

    Primer commit, Se incluye impresiones basicas para el usuario

---
+++ Lista de Tareas/main.py
@@ -0,0 +1,8 @@
+print("--------------Gestor de tareas--------------")
+print("\nQue desa hacer?")
+print("1-Agregar Tarea")
+print("2-Ver Tarea")
+print("3-Marcar Tarea como completada")
+print("4-Eliminar Tarea")
+
+
